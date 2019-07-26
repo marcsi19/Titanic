768692dff7577d819ea87dc2a783d7729ed7295c
Mariann M <mariann19@gmail.com>
2019-07-26 10:12:09 -0700

    initial setup, test

---
+++ src/js/components/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+export default Main;
+const wrapper = document.getElementById("app");
+wrapper ? ReactDOM.render(<Main />, wrapper) : false;
